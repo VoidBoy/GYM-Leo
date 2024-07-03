653313539f23c0a2bb29be57aa7253cb0ec17591
VoidBoy <cubaplus2@gmail.com>
2024-07-03 12:15:04 -0400

    Add files via upload

--- GYM-Leo/main.cpp
+++ GYM-Leo/main.cpp
@@ -3,10 +3,39 @@
 using namespace std;
 
 int opc=0;
+float inmc=0;
 
+void imc_salud(){
+
+    if(inmc<=18.5&&inmc>0){
+        cout<<"Bajo Indice de Masa Corporal \n";
+    }
+    if(inmc>18.5&&inmc<=24.9){
+        cout<<"Indice de Masa Corporal Saludable \n";
+    }
+    if(inmc>24.9&&inmc<=29.9){
+        cout<<"Indice de Masa Corporal Alto \n";
+    }
+    if(inmc>29.9){
+        cout<<"Indice de Masa Corporal Obeso \n";
+    }
+}
+
+//esta funcion calcula el indice de masa corproal
+void imc(){
+    float altura=0;
+    float peso=0;
+
+    cout<<"Digite su altura en metros: \n"; cin>>altura;
+    cout<<"Digite su peso en kilogramos: \n"; cin>>peso;
+
+    inmc = peso / ( altura * altura);
+
+    cout<<"Su indice de masa corporal es de :"<<inmc<<endl<<endl;
+    imc_salud();
+}
+//es el menu principal
 void menu_principal(){
-
-    while (opc!=3) {
 
       if(opc<3&&opc>0){
         switch (opc) {
@@ -14,21 +43,16 @@ void menu_principal(){
 
             break;
         case 2:
-
+               imc();
             break;
-        case 3:break;
         }
           }
       else{
           cout<<"A digitado una opcion incorrecta intente de nuevo";
-          break;
       }
-
-    }
 }
 
-int main()
-{
+int main(){
 
   cout<<"\t\tBienvenido a GYM-Leo:\n\t     Seleccione la opcion deseada\n\n";
 
